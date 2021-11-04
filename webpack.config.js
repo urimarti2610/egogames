b33aff1eb2d31fe9190dfa6bd6ffc40d30595e36
Oriol Martí <omarti@biciescapa.com>
2021-11-04 09:01:49 +0100

    eliminamos postcss-loader porque no se usa

--- webpack.config.js
+++ webpack.config.js
@@ -22,7 +22,6 @@ module.exports = {
                 use: [
                     MiniCSSExtractPlugin.loader,
                     "css-loader",
-                    "postcss-loader",
                     "sass-loader",
                 ]
             },
